445bdc190011913de19dccec702ffcaae629534b
Fathima-Munnath <154072813+Fathima-Munnath@users.noreply.github.com>
2024-09-05 12:43:45 +0530

    first commit
    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function changelogo(){
+    let firstLogo = document.getElementById("logo")
+    firstLogo.setAttribute("src","./images/EntriLogo.jpg")
+}
+
+
+function resetlogo(){
+    let firstLogo = document.getElementById("logo")
+    firstLogo.setAttribute("src","./images/GoogleLogo.png")
+}
